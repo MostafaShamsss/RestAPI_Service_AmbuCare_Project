cc00421fc7c89d5beb537371088c7ec081373eed
Mostafa Shams <mosstafashamss@gmail.com>
2023-04-26 00:45:03 +0200

    cloned into local device

--- src/main/java/com/example/demo/api/controller/LocationController.java
+++ src/main/java/com/example/demo/api/controller/LocationController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -6,12 +5,3 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LocationController {
 }
-=======
-package com.example.demo.api.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LocationController {
-}
->>>>>>> origin/main
